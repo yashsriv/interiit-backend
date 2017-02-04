594d8a7fc666554282036c8d2c5de3589fa89500
Yash Srivastav <admin@yashsriv.org>
2017-02-04 23:20:00 +0530

    Add Forgot Password

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,24 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/users.js');
 var Config = require('../config/hidden.js');
+var querystring = require('querystring');
+var nodemailer = require('nodemailer');
+var bcrypt = require('bcrypt-nodejs');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'interiittech2017@gmail.com',
+    pass: 'kanpurjeetega'
+  }
+});
+var mailOptions = {
+  from: '"Inter IIT 2017" <interiittech2017@gmail.com>',
+  to: '',
+  subject: 'Password Reset',
+  text: '',
+  html: ''
+};
 
 module.exports = function(passport) {
   return router.post('/login', passport.authenticate('local-signin'), function(req, res) { res.json(sanitize(req.user)); })
@@ -43,7 +61,7 @@ module.exports = function(passport) {
         }
       });
     })
-    .get('/logout', isLoggedInn, function(req, res) {
+    .get('/logout', isLoggedIn, function(req, res) {
       req.logout();
     })
     .get('/csv', isAdmin, function(req, res) {
@@ -61,6 +79,59 @@ module.exports = function(passport) {
           res.type('text/csv');
           res.set('Content-Disposition', 'inline; filename="users.csv"');
           res.send(csv);
+        }
+      });
+    })
+    .post('/forgot', function(req, res) {
+      User.find({username: req.body.username}, function(error, user) {
+        if (error) {
+          res.sendStatus(501);
+        } else if (user.length !== 0) {
+          mailOptions.to = user[0].email;
+          var timestamp = Date.now();
+          var hash = user[0].username + ":" + timestamp + ":" + Config.password;
+          var hashed = user[0].generateHash(hash);
+          var link = 'http://interiit.tech/reset?' + querystring.stringify({
+            username: user[0].username,
+            timestamp: timestamp,
+            auth: hashed
+          });
+          mailOptions.text = 'Please click on the following link to restore your password:\n';
+          mailOptions.text += link;
+          mailOptions.html = 'Please click on the following link to restore your passord: <br>';
+          mailOptions.html += '<a href="' + link + '"> Reset Password </a>';
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              res.sendStatus(501);
+            } else {
+              res.sendStatus(200);
+            }
+          });
+        } else {
+          res.sendStatus(404);
+        }
+      });
+    })
+    .post('/reset', function(req, res) {
+      User.find({username: req.body.username}, function(error, user) {
+        if (error) {
+          res.sendStatus(501);
+        } else if (user.length !== 0) {
+          var hash = user[0].username + ':' + req.body.timestamp + ':' + Config.password;
+          if (bcrypt.compareSync(hash, req.body.auth)) {
+            user[0].password = user[0].generateHash(req.body.password);
+            user[0].save(function(err) {
+              if (err) {
+                res.sendStatus(501);
+              } else {
+                res.sendStatus(202);
+              }
+            });
+          } else {
+            res.sendStatus(406);
+          }
+        } else {
+          res.sendStatus(404);
         }
       });
     });
